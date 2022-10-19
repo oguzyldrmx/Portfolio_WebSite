c9e745c8e87a0ced7e29112d4c97de81d9ffe425
Oğuz Yıldırım <oguzyldrm52@gmail.com>
2022-10-19 17:21:32 +0300

    Starting to header section

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {motion} from 'framer-motion';
+import {images} from '../../constants';
 
 import './Header.scss';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="app__header app_flex">
+        
+    </div>
   )
 }
 
