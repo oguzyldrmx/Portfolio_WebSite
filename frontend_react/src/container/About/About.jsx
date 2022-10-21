306bd3668d5a47d902d9a0b9d89e4e574082e544
Oğuz Yıldırım <oguzyldrm52@gmail.com>
2022-10-21 15:44:00 +0300

    Sanity & React

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,19 @@ import React, {useState, useEffect} from 'react';
 import {motion} from 'framer-motion';
 import {images} from '../../constants';
 import './About.scss';
-
-const abouts=[
-  {title:'Web Development', description:'I love web development.', imgUrl: images.about01},
-  {title:'Front-end Development', description:'I am improving myself.', imgUrl: images.about02},
-  {title:'Back-end Development', description:'Back-end is my main occupation.', imgUrl: images.about03},
-  {title:'Team Player', description:'Unity is strength.', imgUrl: images.about04},
-]
+import {urlFor, client} from '../../client';
 
 const About = () => {
+const [abouts, setAbouts] = useState([])
+
+useEffect(() => {
+ const query = '*[_type == "abouts"]';
+
+ client.fetch(query)
+ .then((data) => setAbouts(data))
+}, []);
+
+
   return (
     <>
       <h2 className="head-text">
@@ -26,7 +30,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title+index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{marginTop:20}}>{about.title}</h2>
             <p className="p-text" style={{marginTop:10}}>{about.description}</p>
           </motion.div>
