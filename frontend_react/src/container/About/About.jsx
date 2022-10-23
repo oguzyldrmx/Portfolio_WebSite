d14d5a9e47fba97339a9f0e4ef534c0fb38e9868
Oğuz Yıldırım <oguzyldrm52@gmail.com>
2022-10-23 12:50:20 +0300

    Higher-Order Components

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 
 import {motion} from 'framer-motion';
-import {images} from '../../constants';
 import './About.scss';
 import {urlFor, client} from '../../client';
+import { AppWrap } from '../../wrapper';
+
 
 const About = () => {
 const [abouts, setAbouts] = useState([])
@@ -40,4 +41,4 @@ useEffect(() => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
